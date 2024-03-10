411fde86d2213fb36995648d97cc89399485bdb0
Your Name <kumarvipin897946@gmail.com>
2024-03-10 09:48:41 +0530

    Add project files.

---
+++ Problem Of The Day/Day1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class Day1
+    {
+        static void Main()
+        {
+            //Remove all duplicates from a given string
+
+            int testcases;
+            testcases = Convert.ToInt32(Console.ReadLine());
+            while (testcases-- > 0)
+            {
+                string str = Console.ReadLine().Trim();
+                Day1 obj = new Day1();
+                string res = obj.removeDuplicates(str);
+                Console.WriteLine(res + "\n");
+            }
+            
+        }
+        public string removeDuplicates(string str)
+        {
+            string temp = "";
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (temp.Contains(str[i]) == false)
+                {
+                    temp = temp + str[i];
+                }
+            }
+            return temp;
+        }
+    }
+}
---
+++ Problem Of The Day/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Problem_Of_The_Day
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
