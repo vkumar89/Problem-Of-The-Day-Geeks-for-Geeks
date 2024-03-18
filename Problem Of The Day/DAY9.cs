6d6586816f8b43a49a658ad1abb5ce8b7fdef84a
Your Name <kumarvipin897946@gmail.com>
2024-03-18 07:27:40 +0530

    Day9

---
+++ Problem Of The Day/DAY9.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY9
+    {
+        //Level order traversal
+        //problem Link =>>https://www.geeksforgeeks.org/problems/level-order-traversal/1
+
+        //class Solution
+        //{
+        //    static ArrayList<Integer> levelOrder(Node root)
+        //    {
+
+        //        ArrayList<Integer> result = new ArrayList<>();
+        //        if (root == null) return result;
+        //        Queue<Node> queue = new LinkedList<>();
+        //        queue.add(root);
+        //        while (!queue.isEmpty())
+        //        {
+        //            Node current = queue.poll();
+        //            result.add(current.data);
+        //            if (current.left != null)
+        //                queue.add(current.left);
+        //            if (current.right != null)
+        //                queue.add(current.right);
+        //        }
+        //        return result;
+        //    }
+        //}
+
+    }
+}
