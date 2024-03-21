f7ab621cb96ef2ae859ca3685fd71b9b177c157b
Your Name <kumarvipin897946@gmail.com>
2024-03-21 09:04:45 +0530

    12

---
+++ Problem Of The Day/DAY12.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY12
+    {
+    }
+    //class Solution
+    //{
+    //    public:
+    ////Function to store the zig zag order traversal of tree in a list.
+    //vector<int> zigZagTraversal(Node* root)
+    //    {
+    //        vector<int> ans;
+    //        stack<Node*> s1, s2;
+    //        if (root) s1.push(root);
+
+    //        while (!s1.empty() || !s2.empty())
+    //        {
+    //            while (!s1.empty())
+    //            {
+    //                Node* top = s1.top();
+    //                ans.push_back(top->data);
+    //                s1.pop();
+    //                if (top->left) s2.push(top->left);
+    //                if (top->right) s2.push(top->right);
+    //            }
+    //            while (!s2.empty())
+    //            {
+    //                Node* top = s2.top();
+    //                ans.push_back(top->data);
+    //                s2.pop();
+    //                if (top->right) s1.push(top->right);
+    //                if (top->left) s1.push(top->left);
+    //            }
+    //        }
+    //        return ans;
+    //    }
+    //};
+}
