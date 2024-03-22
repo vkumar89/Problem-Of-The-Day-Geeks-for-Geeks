6358e5ea77c001edcb95c73e1f1dbc2f0eda4f4b
Your Name <kumarvipin897946@gmail.com>
2024-03-22 11:56:21 +0530

    13

---
+++ Problem Of The Day/DAY13.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY13
+    {
+    }
+    //class Solution
+    //{
+    //    private:
+    //void getDiagonalSum(Node* root, vector<int>& ans, int level)
+    //    {
+    //        if (root == NULL)
+    //        {
+    //            return;
+    //        }
+    //        if (ans.size() < level + 1)
+    //        {
+    //            ans.push_back(root->data);
+    //        }
+    //        else
+    //        {
+    //            ans[level] += root->data;
+    //        }
+    //        getDiagonalSum(root->left, ans, level + 1);
+    //        getDiagonalSum(root->right, ans, level);
+    //    }
+    //    public:
+    //vector<int> diagonalSum(Node* root)
+    //    {
+    //        vector<int> ans;
+    //        getDiagonalSum(root, ans, 0);
+    //        return ans;
+    //    }
+    //};
+}
