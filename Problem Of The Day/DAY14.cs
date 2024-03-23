46ea2864c7be318b07016d28fe41a9a019683889
Your Name <kumarvipin897946@gmail.com>
2024-03-23 09:58:59 +0530

    14

---
+++ Problem Of The Day/DAY14.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY14
+    {
+        //Fibonacci series up to Nth term
+        //problem link=>>https://www.geeksforgeeks.org/problems/fibonacci-series-up-to-nth-term/1
+
+    }
+    //class Solution
+    //{
+
+    //    int[] Series(int n)
+    //    {
+    //        int a[] = new int[n + 1];
+    //        a[1] = 1;
+    //        for (int i = 2; i <= n; i++)
+    //        {
+    //            a[i] = (a[i - 1] + a[i - 2]) % 1000000007;
+    //        }
+    //        return a;
+    //    }
+    //}
+}
