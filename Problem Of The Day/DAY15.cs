56bb0a02688a7e5f55eae9a60ea7619e3a29f531
Your Name <kumarvipin897946@gmail.com>
2024-03-24 09:20:30 +0530

    15

---
+++ Problem Of The Day/DAY15.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY15
+    {
+        //problem=>> Insert an Element at the Bottom of a Stack
+
+        //problem link=>>https://www.geeksforgeeks.org/problems/insert-an-element-at-the-bottom-of-a-stack/1
+
+    }
+    //class Solution
+    //{
+    //    public Stack<Integer> insertAtBottom(Stack<Integer> st, int x)
+    //    {
+
+    //        if (st.isEmpty())
+    //        {
+    //            st.push(x);
+    //        }
+    //        else
+    //        {
+    //            int temp = st.pop();
+    //            insertAtBottom(st, x);
+    //            st.push(temp);
+    //        }
+    //        return st;
+    //    }
+    //}
+}
