3feda71a0842c935377e325f610c1e9aa455c8ed
Your Name <kumarvipin897946@gmail.com>
2024-03-29 09:11:44 +0530

    20

---
+++ Problem Of The Day/DAY20.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY20
+    {
+        //Euler Circuit in an Undirected Graph
+
+        //problem link=>>https://www.geeksforgeeks.org/problems/euler-circuit-in-a-directed-graph/1
+    }
+    //class Solution
+    //{
+    //    public boolean isEularCircuitExist(int v, ArrayList<ArrayList<Integer>> adj)
+    //    {
+    //        // Code here
+    //        for (int i = 0; i < adj.size(); i++)
+    //        {
+    //            if (adj.get(i).size() % 2 != 0)
+    //            {
+    //                return false;
+    //            }
+    //        }
+    //        return true;
+    //    }
+    //}
+}
