c9a0869700e3de1eea630325958d1b9934a89694
Your Name <kumarvipin897946@gmail.com>
2024-03-30 08:37:54 +0530

    21

---
+++ Problem Of The Day/DAY21.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY21
+    {
+        //Minimum element in BST
+        //problem link =>>https://www.geeksforgeeks.org/problems/minimum-element-in-bst/1
+    }
+    //class Solution
+    //{
+    //    // Function to find the minimum element in the given BST.
+    //    public
+    //      int minValue(Node root)
+    //    {
+    //        if (root.left == null) return root.data;
+    //        return minValue(root.left);
+    //    }
+    //}
+}
