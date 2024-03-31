debad89948fdb08d63bf7ae8f2c3f75c083df8fe
Your Name <kumarvipin897946@gmail.com>
2024-03-31 11:58:32 +0530

    22

---
+++ Problem Of The Day/DAY22.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY22
+    {
+        //Closest Neighbour in BST
+        //problem link =>>https://www.geeksforgeeks.org/problems/closest-neighbor-in-bst/1
+
+    }
+    //class Solution
+    //{
+    //    public static int findMaxForN(Node root, int n)
+    //    {
+    //        int closest = -1; // Initialize closest as negative infinity
+
+    //        // Traverse the BST
+    //        while (root != null)
+    //        {
+    //            // If current node's key is less than or equal to n, update closest and move to the right subtree
+    //            if (root.key <= n)
+    //            {
+    //                closest = root.key;
+    //                root = root.right;
+    //            }
+    //            // If current node's key is greater than n, move to the left subtree
+    //            else
+    //            {
+    //                root = root.left;
+    //            }
+    //        }
+    //        return closest;
+    //    }
+    //}
+}
