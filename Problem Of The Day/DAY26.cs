81200a98472c2e35a436652937e032300d7b2159
Your Name <kumarvipin897946@gmail.com>
2024-04-04 08:54:41 +0530

    26

---
+++ Problem Of The Day/DAY26.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY26
+    {
+        //Sum of all substrings of a number
+
+        //problem link=>>https://www.geeksforgeeks.org/problems/sum-of-all-substrings-of-a-number-1587115621/1
+    }
+    //class Solution
+    //{
+    //    public static long sumSubstrings(String s)
+    //    {
+    //        //  int modulo = 1000000007;
+    //        int n = s.length();
+    //        long result = 0;
+    //        long multiplier = 1;
+    //        for (int i = n - 1; i >= 0; i--)
+    //        {
+    //            int digit = s.charAt(i) - '0';
+    //            result = (result + (digit * multiplier * (i + 1)) % 1000000007) % 1000000007;
+    //            multiplier = (multiplier * 10 + 1) % 1000000007;
+    //        }
+    //        return result;
+    //    }
+    //}
+}
