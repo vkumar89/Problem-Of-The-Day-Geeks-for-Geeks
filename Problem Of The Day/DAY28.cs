db85953e79b615bd6c131971c211c12d1b931081
Your Name <kumarvipin897946@gmail.com>
2024-04-06 20:53:39 +0530

    28

---
+++ Problem Of The Day/DAY28.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY28
+    {
+        //Count ways to N'th Stair
+        //problem link =>>https://www.geeksforgeeks.org/problems/count-ways-to-nth-stairorder-does-not-matter1322/1
+
+    }
+    //class Solution
+    //{
+    //    // Function to count number of ways to reach the nth stair
+    //    // when order does not matter.
+    //    Long countWays(int n)
+    //    {
+    //        // your code here
+    //        return (long)(Math.floor(n / 2)) + 1L;
+    //    }
+    //}
+}
