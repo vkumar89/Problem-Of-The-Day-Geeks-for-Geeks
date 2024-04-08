14b32d663f270b8a3e534c2307f08be6d74474ef
Your Name <kumarvipin897946@gmail.com>
2024-04-08 18:02:36 +0530

    30

---
+++ Problem Of The Day/DAY30.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY30
+    {
+        //Optimal Strategy For A Game
+
+
+        //problem link=>>https://www.geeksforgeeks.org/problems/optimal-strategy-for-a-game-1587115620/1
+
+    }
+    //class solve
+    //{
+
+    //    static long countMaximum(int n, int arr[])
+    //    {
+
+    //        int[][] dp = new int[n][n];
+
+    //        for (int g = 0; g < n; g++)
+    //        { //g means gap
+    //            for (int i = 0, j = g; j < n; i++, j++)
+    //            {
+    //                if (g == 0) dp[i][j] = arr[i];
+    //                else if (g == 1) dp[i][j] = Math.max(arr[i], arr[j]);
+    //                else
+    //                {
+    //                    dp[i][j] = Math.max(arr[i] + Math.min(dp[i + 2][j], dp[i + 1][j - 1]),
+    //                                      arr[j] + Math.min(dp[i + 1][j - 1], dp[i][j - 2]));
+    //                }
+    //            }
+    //        }
+
+    //        return dp[0][n - 1];
+
+    //    }
+
+    //}
+
+
+
+
+}
