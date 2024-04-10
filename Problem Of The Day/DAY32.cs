f923449da6d15a8198a056204d8746df6bd8e048
Your Name <kumarvipin897946@gmail.com>
2024-04-10 08:15:03 +0530

    32

---
+++ Problem Of The Day/DAY32.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY32
+    {
+        // Party of Couples
+
+        //problem link =>>  https://www.geeksforgeeks.org/problems/alone-in-couple5507/1
+    }
+    //class Solution
+    //{
+    //    static int findSingle(int n, int arr[])
+    //    {
+    //        // code here
+    //        int ans = 0;
+    //        for (int i = 0; i < n; i++)
+    //        {
+    //            ans = ans ^ arr[i];
+    //        }
+    //        return ans;
+    //    }
+    //}
+}
