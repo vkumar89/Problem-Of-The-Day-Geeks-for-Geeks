20ba755129952d797f3295cf8b829a1c319a2b3c
Your Name <kumarvipin897946@gmail.com>
2024-04-11 09:53:39 +0530

    33

---
+++ Problem Of The Day/DAY33.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY33
+    {
+        //Gray to Binary equivalent
+        //problem link =>>https://www.geeksforgeeks.org/problems/gray-to-binary-equivalent-1587115620/1
+    }
+    //class Solution
+    //{
+
+    //    // function to convert a given Gray equivalent n to Binary equivalent.
+    //    public static int grayToBinary(int n)
+    //    {
+
+    //        int binary = 0;
+    //        while (n != 0)
+    //        {
+    //            binary ^= n;
+    //            n >>= 1;
+    //        }
+    //        return binary;
+
+    //    }
+
+    //}
+
+
+}
