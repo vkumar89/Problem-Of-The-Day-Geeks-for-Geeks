2131c6a4b6826ec56a190072852f7671a2d908cc
Your Name <kumarvipin897946@gmail.com>
2024-04-12 09:24:39 +0530

    34

---
+++ Problem Of The Day/DAY34.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY34
+    {
+        //Sum of Products
+
+        //problem link =>>https://www.geeksforgeeks.org/problems/sum-of-products5049/1
+    }
+    //class Solution
+    //{
+    //    //Complete this function
+    //    //Function to calculate and return the sum of the array elements.
+    //    public long pairAndSum(int n, long[] arr)
+    //    {
+    //        long sum = 0; int k = 1;
+
+    //        for (int i = 0; i < 32; i++)
+    //        {
+    //            long c = 0;
+    //            for (int j = 0; j < n; j++)
+    //            {
+    //                if ((arr[j] & 1) != 0)
+    //                {
+    //                    c++;
+    //                }
+    //                arr[j] >>= 1;
+    //            }
+    //            long num = (c * (c - 1)) / 2;
+    //            sum += num * k;
+    //            k <<= 1;
+    //        }
+    //        return sum;
+    //    }
+
+    //}
+}
