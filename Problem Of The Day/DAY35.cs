44d7805c922dcfc3c71fce9ac9ef85d89a75f106
Your Name <kumarvipin897946@gmail.com>
2024-04-13 20:32:20 +0530

    35

---
+++ Problem Of The Day/DAY35.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY35
+    {
+        //Reverse Bits
+
+        //problem Link=>>https://www.geeksforgeeks.org/problems/reverse-bits3556/1
+    }
+    //class Solution
+    //{
+    //    static Long reversedBits(Long x)
+    //    {
+    //        // code here
+    //        long ans = 0;
+    //        for (int i = 0; i < 32; i++)
+    //        {
+    //            int k = 0;
+    //            if (((1 << i) & x) != 0) k++;
+    //            ans += ((1L << 31 - i) * k);
+    //        }
+    //        return ans;
+    //    }
+    //};
+
+
+}
