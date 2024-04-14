05aaff3085be61d139aca8d0efeea301a0781608
Your Name <kumarvipin897946@gmail.com>
2024-04-14 09:39:55 +0530

    36

---
+++ Problem Of The Day/DAY36.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY36
+    {
+        //Xoring and Clearing
+
+        // problem link =>> https://www.geeksforgeeks.org/problems/xoring-and-clearing/1
+
+    }
+    //class Solution
+    //{
+    //    public void printArr(int n, int arr[])
+    //    {
+    //        // code here
+
+    //        for (int a:arr) System.out.print(a + " ");
+
+    //        System.out.println();
+    //    }
+
+    //    public void setToZero(int n, int arr[])
+    //    {
+    //        // code here
+    //        Arrays.fill(arr, 0);
+    //    }
+
+    //    public void xor1ToN(int n, int arr[])
+    //    {
+    //        // code here
+    //        for (int i = 0; i < n; i++)
+    //        {
+    //            arr[i] ^= i;
+    //        }
+    //    }
+    //}
+}
