228abbe3e28e105fb1825483befb3291b109cd46
Your Name <kumarvipin897946@gmail.com>
2024-04-18 09:12:09 +0530

    40

---
+++ Problem Of The Day/DAY40.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY40
+    {
+        //Two Repeated Elements
+        // problem link =>>https://www.geeksforgeeks.org/problems/two-repeated-elements-1587115621/1
+
+    }
+    //class Solution
+    //{
+    //    public int[] twoRepeated(int arr[], int n)
+    //    {
+    //        int[] res = new int[2];
+    //        int[] freq = new int[n + 1];
+    //        for (int a : arr)
+    //        {
+    //            freq[a]++;
+    //            if (freq[a] == 2)
+    //            {
+    //                if (res[0] == 0) res[0] = a;
+    //                else res[1] = a;
+    //            }
+    //        }
+    //        return res;
+    //    }
+
+    //}
+}
