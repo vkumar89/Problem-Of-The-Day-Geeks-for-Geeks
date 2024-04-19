76c4d283dd6c85d9285f5e7113eddded86f1f40f
Your Name <kumarvipin897946@gmail.com>
2024-04-19 14:54:04 +0530

    41

---
+++ Problem Of The Day/DAY41.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY41
+    {
+        //Find missing in second array
+
+        //problem link =>>https://www.geeksforgeeks.org/problems/in-first-but-second5423/1
+    }
+    //class Solution
+    //{
+    //    ArrayList<Integer> findMissing(int a[], int b[], int n, int m)
+    //    {
+    //        ArrayList<Integer> ans = new ArrayList<>();
+    //        HashMap<Integer, Integer> map = new HashMap<>();
+    //        for (int i = 0; i < m; i++)
+    //        {
+    //            map.put(b[i], map.getOrDefault(b[i], 0) + 1);
+    //        }
+    //        for (int i = 0; i < n; i++)
+    //        {
+    //            if (map.get(a[i]) == null) ans.add(a[i]);
+    //            //System.out.println(map.get(a[i]));
+    //        }
+    //        //ans.add(0);
+    //        return ans;
+    //    }
+    //}
+}
