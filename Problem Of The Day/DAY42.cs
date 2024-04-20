eed5f064ee75ca340c18080abdc43d4b94b0de52
Your Name <kumarvipin897946@gmail.com>
2024-04-20 12:21:34 +0530

    42

---
+++ Problem Of The Day/DAY42.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY42
+    {
+        //Union of Two Sorted Arrays
+
+        //problem link =>>https://www.geeksforgeeks.org/problems/union-of-two-sorted-arrays-1587115621/1
+    }
+    //class Solution
+    //{
+    //    public static ArrayList<Integer> findUnion(int arr1[], int arr2[], int n, int m)
+    //    {
+    //        ArrayList<Integer> ans = new ArrayList<>();
+    //        Set<Integer> set = new HashSet<>();
+    //        for (int i:arr1)
+    //        {
+    //            set.add(i);
+    //        }
+    //        for (int i:arr2)
+    //        {
+    //            set.add(i);
+    //        }
+    //        for (int i:set)
+    //        {
+    //            ans.add(i);
+    //        }
+    //        Collections.sort(ans);
+    //        return ans;
+    //    }
+    //}
+}
