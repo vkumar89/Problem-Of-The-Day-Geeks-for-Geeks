d19e126745b49d4a9fd70dd9f39cef928807de3e
Your Name <kumarvipin897946@gmail.com>
2024-04-21 10:24:16 +0530

    43

---
+++ Problem Of The Day/DAY43.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY43
+    {
+        //Three way partitioning
+
+        //problem link =>>https://www.geeksforgeeks.org/problems/three-way-partitioning/1
+    }
+    //class Solution
+    //{
+    //    //Function to partition the array around the range such 
+    //    //that array is divided into three parts.
+    //    public void threeWayPartition(int array[], int a, int b)
+    //    {
+    //        // code here 
+    //        int low = 0;
+    //        int mid = 0;
+    //        int high = array.length - 1;
+    //        while (mid <= high)
+    //        {
+    //            if (array[mid] < a)
+    //            {
+    //                int temp = array[mid];
+    //                array[mid] = array[low];
+    //                array[low] = temp;
+    //                low++;
+    //                mid++;
+    //            }
+    //            else if (array[mid] > b)
+    //            {
+    //                int temp = array[mid];
+    //                array[mid] = array[high];
+    //                array[high] = temp;
+    //                high--;
+    //            }
+    //            else
+    //            {
+    //                mid++;
+    //            }
+    //        }
+    //    }
+    //}
+}
