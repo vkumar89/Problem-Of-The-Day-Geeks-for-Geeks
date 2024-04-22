b1f9ebac531e575d02c454597f1b7d28c186478a
Your Name <kumarvipin897946@gmail.com>
2024-04-22 08:29:47 +0530

    44

---
+++ Problem Of The Day/DAY44.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY44
+    {
+        //Row with minimum number of 1's
+
+        // problem link =>>https://www.geeksforgeeks.org/problems/row-with-minimum-number-of-1s5430/1
+    }
+    //class Solution
+    //{
+    //    int minRow(int n, int m, int a[][])
+    //    {
+    //        // code here
+    //        int mr = 0;
+    //        int min = Integer.MAX_VALUE;
+    //        for (int i = 0; i < n; i++)
+    //        {
+
+    //            int r = 0;
+    //            for (int j = 0; j < m; j++)
+    //            {
+
+    //                if (a[i][j] == 1)
+    //                {
+    //                    r++;
+    //                }
+    //            }
+    //            int t = min;
+    //            min = Math.min(min, r);
+    //            if (t != min)
+    //            {
+    //                mr = i + 1;
+    //            }
+    //        }
+    //        return mr != 0 ? mr : 1;
+    //    }
+    //}
+}
