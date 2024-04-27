523559798474dc131ed5e49002c8b1703a129373
Your Name <kumarvipin897946@gmail.com>
2024-04-27 08:51:31 +0530

    49

---
+++ Problem Of The Day/DAY49.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY49
+    {
+        //Merge Sort on Doubly Linked List
+
+        //problem link=>>https://www.geeksforgeeks.org/problems/merge-sort-on-doubly-linked-list/1
+    }
+
+    //    class Solution
+    //    {
+    //        public:
+    //    // Function to sort the given doubly linked list using Merge Sort.
+    //    struct Node *sortDoubly(struct Node *head) {
+    //     vector<int> num;
+    //        struct Node *temp = head;
+    //        while(temp != NULL){
+    //            num.push_back(temp->data);
+    //            temp = temp->next;
+    //        }
+    //    sort(num.begin(), num.end());
+
+    //    temp = head;
+    //        int ind = 0;
+    //        while(temp){
+    //            temp->data = num[ind++];
+    //            temp = temp->next;
+    //        }
+    //return head;
+    //    }
+    //};
+}
