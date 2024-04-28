13f3f0d50cde66389671fa0d2d95c527f257329d
Your Name <kumarvipin897946@gmail.com>
2024-04-28 08:39:09 +0530

    50

---
+++ Problem Of The Day/DAY50.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY50
+    {
+        //Delete Middle of Linked List
+        // problem link =>>https://www.geeksforgeeks.org/problems/delete-middle-of-linked-list/1
+    }
+    //class Solution
+    //{
+    //    public:
+    //Node* deleteMid(Node* head)
+    //    {
+    //        Node* slow = head;
+    //        Node* fast = head;
+    //        Node* prev = nullptr;
+    //        while (fast->next)
+    //        {
+    //            prev = slow;
+    //            slow = slow->next;
+    //            if (fast->next->next) fast = fast->next->next;
+    //            else { break; }
+    //        }
+    //        prev->next = slow->next;
+    //        return head;
+    //    }
+    //};
+}
