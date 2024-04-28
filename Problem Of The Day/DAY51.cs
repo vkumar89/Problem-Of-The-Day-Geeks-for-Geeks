bcf7211270cd606690b43ccc4de9d2e6226e1965
Your Name <kumarvipin897946@gmail.com>
2024-04-29 01:09:49 +0530

    51

---
+++ Problem Of The Day/DAY51.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY51
+    {
+        // Remove every kth node
+        //problem link=>>https://www.geeksforgeeks.org/problems/remove-every-kth-node/1
+    }
+    //class Solution
+    //{
+    //    public:
+    //  Node* deleteK(Node* head, int K)
+    //    {
+    //        //Your code here
+    //        if (K == 1) return nullptr;
+    //        Node* prev = nullptr;
+    //        Node* temp = head;
+    //        int count = 0;
+    //        while (temp != nullptr)
+    //        {
+    //            count++;
+    //            if (count == K)
+    //            {
+    //                prev->next = temp->next;
+    //                delete(temp);
+    //                if (prev->next != nullptr)
+    //                    temp = prev->next;
+    //                count = 0;
+    //                continue;
+    //            }
+
+    //            prev = temp;
+    //            temp = temp->next;
+    //        }
+    //        return head;
+    //    }
+    //};
+
+}
