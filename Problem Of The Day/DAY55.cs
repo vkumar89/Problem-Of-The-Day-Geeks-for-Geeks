799cdc389002e3a84eea77eac290fff229671d79
Your Name <kumarvipin897946@gmail.com>
2024-05-03 05:27:12 +0530

    55

---
+++ Problem Of The Day/DAY55.cs
@@ -0,0 +1,47 @@
+﻿using Problem_Of_The_Day;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY55
+    {
+        //K distance from root
+        //problem link =>>https://www.geeksforgeeks.org/problems/k-distance-from-root/1
+    }
+    //class Solution
+    //{
+    //    public:
+    //vector<int> Kdistance(struct Node *root, int k) {
+    //    queue<Node*> q;
+    //    q.push(root);
+
+    //    for(int i = 0; i<k && !q.empty(); i++) {
+    //        int n = q.size();
+
+    //        while(n--) {
+    //            Node* f = q.front();
+    //    q.pop();
+
+    //            if(f->left) q.push(f->left);
+    //            if(f->right) q.push(f->right);
+    //        }
+}
+
+//vector<int> ans;
+//while (!q.empty())
+//{
+//    Node* f = q.front();
+//    q.pop();
+
+//    ans.push_back(f->data);
+//}
+
+//return ans;
+//    }
+//};
+//}
