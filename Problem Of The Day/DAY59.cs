bb60e3548c77f563a4b3812bc5a8a50462e4dcdd
Your Name <kumarvipin897946@gmail.com>
2024-05-07 09:48:22 +0530

    59

---
+++ Problem Of The Day/DAY59.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY59
+    {
+        //Reverse Level Order Traversal
+
+        //problem link =>> https://www.geeksforgeeks.org/problems/reverse-level-order-traversal/1
+    }
+    //class Tree
+    //{
+    //    public ArrayList<Integer> reverseLevelOrder(Node node)
+    //    {
+    //        // code here
+    //        ArrayList<Integer> ans = new ArrayList<>();
+    //        Queue<Node> q = new LinkedList<>();
+    //        q.offer(node);
+    //        while (!q.isEmpty())
+    //        {
+    //            Node cur = q.poll();
+    //            ans.add(0, cur.data);
+    //            if (cur.right != null) q.offer(cur.right);
+    //            if (cur.left != null) q.offer(cur.left);
+
+    //        }
+    //        return ans;
+    //    }
+    //}
+}
