e0fe7879938cf6984c5d3a95ec5403bcdd3bb160
Your Name <kumarvipin897946@gmail.com>
2024-05-09 09:34:10 +0530

    61

---
+++ Problem Of The Day/DAY61.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY61
+    {
+
+        //Divisor Game
+        //problem link=>> https://www.geeksforgeeks.org/problems/divisor-game-1664432414/1
+    }
+
+    //class Solution
+    //{
+    //    // Complete this function
+    //    public bool divisorGame(int n)
+    //    {
+    //        return n % 2 == 0 ? true : false;
+    //    }
+    //}
+}
