276c2c665303323d8572d0e902505a9b044f1830
Your Name <kumarvipin897946@gmail.com>
2024-05-11 10:05:37 +0530

    63

---
+++ Problem Of The Day/DAY63.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY63
+    {
+        //Juggler Sequence
+
+        //problem link=>>https://www.geeksforgeeks.org/problems/juggler-sequence3930/1
+    }
+    //class Solution
+    //{
+    //    // Complete this function
+    //    public List<int> jugglerSequence(int n)
+    //    {
+
+    //        List<int> res = new List<int>();
+
+    //        while (n != 1)
+    //        {
+    //            res.Add(n);
+
+    //            if (n % 2 == 0)
+    //                n = (int)Math.Sqrt(n);
+    //            else
+    //                n = (int)Math.Pow(Math.Sqrt(n), 3);
+    //        }
+
+    //        res.Add(1);
+    //        return res;
+
+    //    }
+}
