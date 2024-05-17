78b21a2dc8af24a4ff5f91515847a205030d107c
Your Name <kumarvipin897946@gmail.com>
2024-05-17 07:16:24 +0530

    69

---
+++ Problem Of The Day/DAY69.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY69
+    {
+       // Find Pair Given Difference
+
+        //problem link=>> https://www.geeksforgeeks.org/problems/find-pair-given-difference1559/1
+    }
+    //class Solution
+    //{
+    //    public int findPair(int n, int x, int[] arr)
+    //    {
+    //        Set<Integer> c = new HashSet<>();
+    //        for (int i : arr)
+    //        {
+    //            if (c.contains(i + x) || c.contains(i - x))
+    //            {
+    //                return 1;
+    //            }
+    //            c.add(i);
+    //        }
+    //        return -1;
+    //    }
+    //}
+}
