683d7a6127db89a290bd03cd7698406c16fd018e
Your Name <kumarvipin897946@gmail.com>
2024-05-18 10:11:03 +0530

    70

---
+++ Problem Of The Day/DAY70.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY70
+    {
+        //Find the Highest number
+        // problem link =>>https://www.geeksforgeeks.org/problems/find-the-highest-number2259/1
+
+
+    }
+    //class Solution
+    //{
+    //    public int findPeakElement(List<Integer> a)
+    //    {
+    //        int ans = Integer.MIN_VALUE;
+    //        int start = 0;
+    //        int end = a.size();
+    //        while (start < end)
+    //        {
+    //            int mid = (start + end) / 2;
+    //            if (a.get(mid) < a.get(end - 1)) start = mid;
+    //            else end = mid;
+    //            ans = Math.max(ans, a.get(mid));
+    //        }
+    //        return ans;
+    //    }
+    //}
+}
