bf03c2a05993af6c03751fca566a58146c2e23d9
Your Name <kumarvipin897946@gmail.com>
2024-05-19 09:51:35 +0530

    71

---
+++ Problem Of The Day/DAY71.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY71
+    {
+        //Find the closest number
+
+        //problem link=>>https://www.geeksforgeeks.org/problems/find-the-closest-number5513/1
+    }
+
+    //class Solution
+    //{
+    //    public static int findClosest(int n, int k, int[] arr)
+    //    {
+    //        int r = arr[0];
+    //        int t = Math.abs(arr[0] - k);
+    //        for (int x = 1; x < arr.length; x++)
+    //        {
+    //            int p = Math.abs(arr[x] - k);
+    //            if ((p < t) || (p == t && arr[x] > r))
+    //            {
+    //                t = p;
+    //                r = arr[x];
+    //            }
+    //        }
+    //        return r;
+    //    }
+    //}
+}
