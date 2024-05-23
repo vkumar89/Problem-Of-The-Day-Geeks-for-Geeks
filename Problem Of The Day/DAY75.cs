92dc73d4f5e7b5ffa71798a96094914a947f003e
Your Name <kumarvipin897946@gmail.com>
2024-05-23 08:15:52 +0530

    75

---
+++ Problem Of The Day/DAY75.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY75
+    {
+        //K-Palindrome
+        //problem link =>> https://www.geeksforgeeks.org/problems/find-if-string-is-k-palindrome-or-not1923/1
+    }
+    //class Solution
+    //{
+    //    public int kPalindrome(String str, int n, int k)
+    //    {
+    //        int[][] dp = new int[n][n];
+
+    //        // Fill the DP table
+    //        for (int length = 2; length <= n; length++)
+    //        {  // Length of the substring
+    //            for (int i = 0; i <= n - length; i++)
+    //            {
+    //                int j = i + length - 1;
+    //                if (str.charAt(i) == str.charAt(j))
+    //                {
+    //                    dp[i][j] = dp[i + 1][j - 1];
+    //                }
+    //                else
+    //                {
+    //                    dp[i][j] = Math.min(dp[i + 1][j], dp[i][j - 1]) + 1;
+    //                }
+    //            }
+    //        }
+
+    //        // The result is in dp[0][n-1]
+    //        return dp[0][n - 1] <= k ? 1 : 0;
+    //    }
+    //}
+}
