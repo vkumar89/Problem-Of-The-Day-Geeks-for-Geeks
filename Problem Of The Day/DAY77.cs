c6ce3efc83f9935399f54fa914ac16a9e6f67597
Your Name <kumarvipin897946@gmail.com>
2024-05-25 20:51:05 +0530

    77

---
+++ Problem Of The Day/DAY77.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY77
+    {
+        //You and your books
+        //problem link https://www.geeksforgeeks.org/problems/you-and-your-books/1
+    }
+
+    //class Solution
+    //{
+    //    // Complete this function
+    //    public int max_Books(int[] arr, int n, int k)
+    //    {
+
+    //        int curr = 0, ans = 0;
+
+    //        for (int i = 0; i < n; i++)
+    //        {
+    //            if (arr[i] <= k)
+    //            {
+    //                curr += arr[i];
+    //            }
+    //            else
+    //            {
+    //                curr = 0;
+    //            }
+    //            ans = Math.Max(ans, curr);
+    //        }
+    //        return ans;
+
+    //    }
+    //}
+}
