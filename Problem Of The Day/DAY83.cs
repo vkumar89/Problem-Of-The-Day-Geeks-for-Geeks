d199c85da87c41858289840ea2a89568d8b72822
Your Name <kumarvipin897946@gmail.com>
2024-05-31 09:03:42 +0530

    83

---
+++ Problem Of The Day/DAY83.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY83
+    {
+        //Swap two nibbles in a byte
+        //problem link =>> https://www.geeksforgeeks.org/problems/swap-two-nibbles-in-a-byte0446/1
+
+    }
+    //class Solution
+    //{
+    //    static int swapNibbles(int n)
+    //    {
+    //        return (n >> 4) | ((n & 15) << 4);
+    //    }
+    //}
+}
