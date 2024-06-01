2731b8ec60f73ab3ad14170075655bfa21e4b279
Your Name <kumarvipin897946@gmail.com>
2024-06-01 20:04:41 +0530

    84

---
+++ Problem Of The Day/DAY84.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY84
+    {
+        //Odd Even Problem
+        //problem link =>> https://www.geeksforgeeks.org/problems/help-nobita0532/1
+    }
+
+    //class Solution
+    //{
+    //    public static String oddEven(String s)
+    //    {
+    //        HashMap<Character, Integer> m = new HashMap<>();
+    //        for (char ch:s.toCharArray()) m.put(ch, m.getOrDefault(ch, 0) + 1);
+    //        int x = 0, y = 0;
+    //        for (char ch:m.keySet())
+    //        {
+    //            if (m.get(ch) % 2 == 0 && (ch - '0') % 2 == 0) x++;
+    //            if (m.get(ch) % 2 != 0 && (ch - '0') % 2 != 0) y++;
+
+    //        }
+    //        return (x + y) % 2 == 0 ? "EVEN" : "ODD";
+    //    }
+    //}
+
+}
