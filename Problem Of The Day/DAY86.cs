13217c7e26e7c74b9750f7ee4adfd9cc432d817e
Your Name <kumarvipin897946@gmail.com>
2024-06-03 09:44:42 +0530

    86

---
+++ Problem Of The Day/DAY86.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY86
+    {
+        //Trail of ones
+        //problem link =>> https://www.geeksforgeeks.org/problems/trail-of-ones3242/1
+    }
+    //class Solution
+    //{
+    //    static int numberOfConsecutiveOnes(int n)
+    //    {
+    //        int MOD = 1000000007;
+
+    //        int[] dp = new int[n + 1];
+
+    //        dp[0] = 0;
+    //        dp[1] = 0;
+    //        dp[2] = 1;
+
+    //        if (n < 3)
+    //        {
+    //            return dp[n];
+    //        }
+
+    //        int a = 0;
+    //        int b = 1;
+
+    //        for (int i = 3; i <= n; i++)
+    //        {
+    //            int c = (a + b) % MOD;
+    //            dp[i] = (dp[i - 1] * 2) % MOD + c % MOD;
+    //            dp[i] = dp[i] % MOD;
+    //            a = b;
+    //            b = c;
+    //        }
+    //        return dp[n];
+    //    }
+    //}
+}
