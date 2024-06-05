ad8d096ca707364938c821288b2e41f163309171
Your Name <kumarvipin897946@gmail.com>
2024-06-05 10:25:24 +0530

    88

---
+++ Problem Of The Day/DAY88.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY88
+    {
+        //Swapping pairs make sum equal
+        //problem link =>>https://www.geeksforgeeks.org/problems/swapping-pairs-make-sum-equal4142/1
+    }
+    //class Solution
+    //{
+    //    long findSwapValues(long A[], int n, long B[], int m)
+    //    {
+    //        long sumA = 0, sumB = 0;
+    //        for (long ele:A)
+    //            sumA += ele;
+
+    //        for (long ele:B)
+    //            sumB += ele;
+
+    //        if ((sumA - sumB) % 2 != 0)
+    //            return -1;
+    //        long target = (sumA - sumB) / 2;
+
+    //        HashSet<Long> hs = new HashSet<>();
+    //        for (long ele: A)
+    //            hs.add(ele);
+
+    //        for (long ele: B)
+    //        {
+
+    //            if (hs.contains(target + ele))
+    //                return 1;
+    //        }
+    //        return -1;
+    //    }
+    //}
+
+}
