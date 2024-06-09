4e9d97fd742dd1f082572dcc5420a73cd6473321
Your Name <kumarvipin897946@gmail.com>
2024-06-09 09:44:47 +0530

    92

---
+++ GeeksForGeeks/Problem Of The Day/DAY92.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY92
+    {
+        //Convert array into Zig-Zag fashion
+        //problem link =>>https://www.geeksforgeeks.org/problems/convert-array-into-zig-zag-fashion1638/1
+
+    }
+    //class Solution
+    //{
+    //    public static void zigZag(int n, int[] arr)
+    //    {
+    //        // code here
+    //        for (int i = 1; i < n; i++)
+    //        {
+    //            if (i % 2 == 1 && arr[i] < arr[i - 1])
+    //            {
+    //                int temp = arr[i];
+    //                arr[i] = arr[i - 1];
+    //                arr[i - 1] = temp;
+    //            }
+    //            else if (i % 2 == 0 && arr[i] > arr[i - 1])
+    //            {
+    //                int temp = arr[i];
+    //                arr[i] = arr[i - 1];
+    //                arr[i - 1] = temp;
+    //            }
+    //        }
+    //    }
+    //}
+
+
+}
