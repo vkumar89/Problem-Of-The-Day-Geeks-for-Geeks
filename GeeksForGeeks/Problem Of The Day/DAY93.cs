e512d4387e4b9be84448cd8994483c967cb7a7a6
Your Name <kumarvipin897946@gmail.com>
2024-06-10 06:35:27 +0530

    93

---
+++ GeeksForGeeks/Problem Of The Day/DAY93.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY93
+    {
+        //Nuts and Bolts Problem
+        //problem link=>> https://www.geeksforgeeks.org/problems/nuts-and-bolts-problem0431/1
+    }
+//    class Solution
+//    {
+//        public static final HashMap<Character, Integer> map= new HashMap<>();
+//        static{
+//        map.put('!',1);
+//        map.put('#',2);
+//        map.put('$',3);
+//        map.put('%',4);
+//        map.put('&',5);
+//        map.put('*',6);
+//        map.put('?',7);
+//        map.put('@',8);
+//        map.put('^',9);
+//    }
+
+
+//    static int cmp(char n)
+//    {
+//        return map.get(n);
+//    }
+
+//    void matchPairs(int n, char nuts[], char bolts[])
+//    {
+//        // code here
+//        Character[] nutChars = new Character[n];
+//        for (int i = 0; i < n; i++)
+//        {
+//            nutChars[i] = nuts[i];
+//        }
+
+//        Character[] boltChars = new Character[n];
+//        for (int i = 0; i < n; i++)
+//        {
+//            boltChars[i] = bolts[i];
+//        }
+//        Arrays.sort(nutChars, (a, b)->cmp(a) - cmp(b));
+//        Arrays.sort(boltChars, (a, b)->cmp(a) - cmp(b));
+//        for (int i = 0; i < n; i++)
+//        {
+//            nuts[i] = nutChars[i];
+//            bolts[i] = boltChars[i];
+//        }
+
+//    }
+//}
+}
