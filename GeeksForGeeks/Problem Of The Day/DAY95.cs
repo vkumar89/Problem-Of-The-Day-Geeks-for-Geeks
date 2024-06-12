3bd6b6e8efafa0e32d592c794cfa8cf27316e07d
Your Name <kumarvipin897946@gmail.com>
2024-06-12 20:40:17 +0530

    95

---
+++ GeeksForGeeks/Problem Of The Day/DAY95.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY95
+    {
+        //Count numbers containing 4
+        //problem link =>>https://www.geeksforgeeks.org/problems/count-numbers-containing-43022/1
+    }
+
+    //class Solution
+    //{
+    //    public static int countNumberswith4(int n)
+    //    {
+    //        int count = 0;
+    //        for (int i = 1; i <= n; i++)
+    //        {
+    //            if (Integer.toString(i).contains("4")) count++;
+    //        }
+    //        return count;
+    //    }
+    //}
+
+}
