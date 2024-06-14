ab16f1eccd9678fc141e98da9fbe55f8559d7333
Your Name <kumarvipin897946@gmail.com>
2024-06-14 18:51:42 +0530

    97

---
+++ GeeksForGeeks/Problem Of The Day/DAY97.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY97
+    {
+        //Armstrong Numbers
+        //problem link =>>https://www.geeksforgeeks.org/problems/armstrong-numbers2727/1
+    }
+    //class Solution
+    //{
+    //    static String armstrongNumber(int n)
+    //    {
+    //        // code here
+    //        int originalNumber = n;
+    //        int sumOfCubes = 0;
+
+    //        while (n > 0)
+    //        {
+    //            int digit = n % 10;
+    //            sumOfCubes += digit * digit * digit;
+    //            n /= 10;
+    //        }
+    //        if (sumOfCubes == originalNumber)
+    //        {
+
+    //            return "true";
+    //        }
+    //        else
+    //        {
+    //            return "false";
+    //        }
+    //    }
+    //}
+}
