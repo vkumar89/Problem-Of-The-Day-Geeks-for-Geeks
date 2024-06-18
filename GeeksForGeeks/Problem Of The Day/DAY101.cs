4af0ea561de03c01222d1815ed72e12b0189dc3b
Your Name <kumarvipin897946@gmail.com>
2024-06-18 09:20:35 +0530

    101

---
+++ GeeksForGeeks/Problem Of The Day/DAY101.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY101
+    {
+        //Number of Rectangles in a Circle
+        //problem link =>>https://www.geeksforgeeks.org/problems/rectangles-in-a-circle0457/1
+
+    }
+
+    //class Solution
+    //{
+    //    int rectanglesInCircle(int r)
+    //    {
+    //        int count = 0;
+    //        int diameterSquared = 4 * r * r;
+    //        for (int w = 1; w <= 2 * r; w++)
+    //        {
+    //            for (int h = 1; h <= 2 * r; h++)
+    //            {
+    //                if (w * w + h * h <= diameterSquared)
+    //                {
+    //                    count++;
+    //                }
+    //            }
+    //        }
+    //        return count;
+    //    }
+    //};
+}
