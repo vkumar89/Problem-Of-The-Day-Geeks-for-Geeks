a6ae630b98fe36cb2b91c26caf3e454eca957973
Your Name <kumarvipin897946@gmail.com>
2024-06-19 17:09:14 +0530

    102

---
+++ GeeksForGeeks/Problem Of The Day/DAY102.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY102
+    {
+        //Find maximum volume of a cuboid
+        //problem link=>>https://www.geeksforgeeks.org/problems/magical-box5306/1
+    }
+    //class Solution
+    //{
+
+    //    double maxVolume(double perimeter, double area)
+    //    {
+    //        double l = (double)(perimeter - Math.sqrt(perimeter * perimeter - 24 * area)) / 12;
+
+    //        double height = (perimeter / 4) - (2 * (l));
+
+    //        double ans = l * l * height;
+    //        return ans;
+    //    }
+    //}
+}
