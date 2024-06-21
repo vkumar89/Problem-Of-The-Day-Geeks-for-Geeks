3c84f4fbc2f9a4aac36efd92c43c0e65fd8b8c2d
Your Name <kumarvipin897946@gmail.com>
2024-06-21 11:04:19 +0530

    104

---
+++ GeeksForGeeks/Problem Of The Day/DAY104.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY104
+    {
+        //Compare two fractions
+        //problem link=>>https://www.geeksforgeeks.org/problems/compare-two-fractions4438/1
+
+    }
+    //class Solution
+    //{
+
+    //    String compareFrac(String a)
+    //    {
+
+    //        Double n1 = Double.parseDouble(a.split(",")[0].split("/")[0]);
+    //        Double n12 = Double.parseDouble(a.split(",")[0].split("/")[1]);
+    //        Double n2 = Double.parseDouble(a.split(",")[1].substring(1).split("/")[0]);
+    //        Double n22 = Double.parseDouble(a.split(",")[1].substring(1).split("/")[1]);
+
+    //        if ((n1 / n12) > (n2 / n22)) return a.split(",")[0];
+    //        if ((n1 / n12) < (n2 / n22)) return a.split(",")[1].substring(1);
+
+    //        return "equal";
+    //    }
+    //}
+}
