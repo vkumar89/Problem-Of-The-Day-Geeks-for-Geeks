260b95a79511bc8d524925c60699e0f727a7aa18
Your Name <kumarvipin897946@gmail.com>
2024-06-23 08:00:40 +0530

    106

---
+++ GeeksForGeeks/Problem Of The Day/DAY106.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY106
+    {
+        //Print Bracket Number
+        //problem  link=>>https://www.geeksforgeeks.org/problems/print-bracket-number4058/1
+    }
+    //class Solution
+    //{
+    //    ArrayList<Integer> bracketNumbers(String str)
+    //    {
+
+    //        int open = 0;
+
+
+    //        Stack<Integer> stack = new Stack<Integer>();
+    //        ArrayList<Integer> arr = new ArrayList<Integer>();
+
+    //        for (int i = 0; i < str.length(); i++)
+    //        {
+    //            if (str.charAt(i) == '(')
+    //            {
+    //                arr.add(++open);
+    //                stack.push(open);
+    //            }
+    //            else if (str.charAt(i) == ')')
+    //            {
+    //                arr.add(stack.pop());
+    //            }
+    //        }
+
+    //        return arr;
+    //    }
+    //};
+}
