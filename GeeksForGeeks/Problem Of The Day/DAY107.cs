ba89b2a2c935af9f3c5f153777de9a7c29332dcb
Your Name <kumarvipin897946@gmail.com>
2024-06-24 09:05:08 +0530

    107

---
+++ GeeksForGeeks/Problem Of The Day/DAY107.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY107
+    {
+        //Summed Matrix
+        //problem link =>>https://www.geeksforgeeks.org/problems/summed-matrix5834/1
+    }
+
+    //class Solution
+    //{
+    //    static long sumMatrix(long n, long q)
+    //    {
+    //        return Math.max(0, n - Math.abs(n - q + 1));
+    //    }
+    //}
+}
