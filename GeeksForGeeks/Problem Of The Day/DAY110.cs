38f2d36749a453f9d0093b95b881f49a364ff1e4
Your Name <kumarvipin897946@gmail.com>
2024-06-27 13:11:46 +0530

    110

---
+++ GeeksForGeeks/Problem Of The Day/DAY110.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY110
+    {
+        //Toeplitz matrix
+        //problem link =>>https://www.geeksforgeeks.org/problems/toeplitz-matrix/1
+    }
+    //class Solution
+    //{
+    //    /*You are required to complete this method*/
+    //    boolean isToeplitz(int mat[][])
+    //    {
+    //        int n = mat.length;
+    //        int m = mat[0].length;
+    //        for (int i = 1; i < n; i++)
+    //        {
+    //            for (int j = 1; j < m; j++)
+    //            {
+    //                if (mat[i][j] != mat[i - 1][j - 1]) return false;
+    //            }
+    //        }
+    //        return true;
+    //    }
+    //}
+}
