9b4b2de4698d6e35fc4847debdebd5193e9a8b80
Your Name <kumarvipin897946@gmail.com>
2024-06-28 10:02:35 +0530

    111

---
+++ GeeksForGeeks/Problem Of The Day/DAY111.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY111
+    {
+        //The Palindrome Pattern
+        //problem link=>>https://www.geeksforgeeks.org/problems/the-palindrome-pattern3900/1
+
+    }
+
+    //class Solution
+    //{
+    //    public String pattern(int[][] arr)
+    //    {
+    //        // Code here
+    //        for (int r = 0; r < arr.length; r++)
+    //        {
+    //            int s = 0, e = arr.length - 1;
+    //            boolean isPalin = true;
+    //            while (s < e)
+    //            {
+    //                if (arr[r][s] != arr[r][e])
+    //                {
+    //                    isPalin = false;
+    //                    break;
+    //                }
+    //                s++; e--;
+    //            }
+    //            if (isPalin) return r + " R";
+    //        }
+    //        for (int c = 0; c < arr.length; c++)
+    //        {
+    //            int s = 0, e = arr.length - 1;
+    //            boolean isPalin = true;
+    //            while (s < e)
+    //            {
+    //                if (arr[s][c] != arr[e][c])
+    //                {
+    //                    isPalin = false;
+    //                    break;
+    //                }
+    //                s++; e--;
+    //            }
+    //            if (isPalin) return c + " C";
+    //        }
+    //        return "-1";
+    //    }
+    //}
+
+}
