8302125b74f7e4e88a36bf24335956bfc9e0f0b2
Your Name <kumarvipin897946@gmail.com>
2024-06-30 03:29:58 +0530

    113

---
+++ GeeksForGeeks/Problem Of The Day/DAY113.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY113
+    {
+        //Delete node in Doubly Linked List
+        //problem link=>>https://www.geeksforgeeks.org/problems/delete-node-in-doubly-linked-list/1
+    }
+    //class Solution
+    //{
+    //    public Node deleteNode(Node head, int x)
+    //    {
+    //        if (x == 1)
+    //        {
+    //            Node temp = head;
+    //            temp = temp.next;
+    //            head = temp;
+    //            return head;
+    //        }
+    //        else
+    //        {
+    //            Node temp = head;
+    //            for (int i = 1; i < x; i++)
+    //                temp = temp.next;
+    //            if (temp.next == null)
+    //            {
+    //                temp = temp.prev;
+    //                temp.next = null;
+    //            }
+    //            else
+    //                temp.prev.next = temp.next;
+    //        }
+    //        return head;
+    //    }
+    //}
+
+}
