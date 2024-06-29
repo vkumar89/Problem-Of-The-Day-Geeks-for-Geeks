c8d202c9575949feaeaf25386c731e443f20a77d
Your Name <kumarvipin897946@gmail.com>
2024-06-29 20:34:42 +0530

    112

---
+++ GeeksForGeeks/Problem Of The Day/DAY112.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY112
+    {
+        //Identical Linked Lists
+        //problem link=>>https://www.geeksforgeeks.org/problems/identical-linked-lists/1
+    }
+
+    //class Solution
+    //{
+    //    public boolean areIdentical(Node head1, Node head2)
+    //    {
+
+    //        while (head1 != null && head2 != null)
+    //        {
+    //            if (head1.data != head2.data)
+    //            {
+    //                return false;
+    //            }
+    //            head1 = head1.next;
+    //            head2 = head2.next;
+    //        }
+
+    //        if (head1 == null && head2 == null)
+    //            return true;
+
+    //        return false;
+    //    }
+    //}
+}
