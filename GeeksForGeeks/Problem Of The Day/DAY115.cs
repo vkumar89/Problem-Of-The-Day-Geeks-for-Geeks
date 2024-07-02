b4e77d47ec782d44a7111b38fccc6fc36b11771e
Your Name <kumarvipin897946@gmail.com>
2024-07-02 21:47:44 +0530

    115

---
+++ GeeksForGeeks/Problem Of The Day/DAY115.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY115
+    {
+        //Linked list of strings forms a palindrome
+        //problem link =>>https://www.geeksforgeeks.org/problems/linked-list-of-strings-forms-a-palindrome/1
+    }
+
+    //class Solution
+    //{
+    //    public boolean compute(Node root)
+    //    {
+    //        StringBuilder combinedString = new StringBuilder();
+    //        for (Node current = root; current != null; current = current.next)
+    //        {
+    //            combinedString.append(current.data);
+    //        }
+
+    //        String str = combinedString.toString();
+    //        int n = str.length();
+    //        for (int i = 0; i < n / 2; i++)
+    //        {
+    //            if (str.charAt(i) != str.charAt(n - i - 1))
+    //            {
+    //                return false;
+    //            }
+    //        }
+    //        return true;
+    //    }
+    //}
+}
