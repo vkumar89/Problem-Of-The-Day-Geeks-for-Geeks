00bf53ab5fd40feab2a67a272f2918f538c93e5f
Your Name <kumarvipin897946@gmail.com>
2024-07-08 21:40:43 +0530

    121

---
+++ GeeksForGeeks/Problem Of The Day/DAY121.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY121
+    {
+        //Search in Rotated Sorted Array
+        //problem link =>>https://www.geeksforgeeks.org/problems/search-in-a-rotated-array4618/1
+    }
+    //class Solution
+    //{
+    //    int search(int[] arr, int key)
+    //    {
+
+    //        for (int i = 0; i < arr.length; i++)
+    //        {
+    //            if (arr[i] == key)
+    //            {
+    //                return i;
+    //            }
+    //        }
+    //        return -1;
+    //    }
+    //}
+}
