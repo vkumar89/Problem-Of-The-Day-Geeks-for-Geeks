fd6e5a93ee93d258871dee70276cf25934727b0e
Your Name <kumarvipin897946@gmail.com>
2024-07-12 22:03:38 +0530

    125

---
+++ GeeksForGeeks/Problem Of The Day/DAY125.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY125
+    {
+        //Root to leaf path sum
+        //problem link =>>https://www.geeksforgeeks.org/problems/root-to-leaf-path-sum/1
+    }
+    //class Solution
+    //{
+        
+    //    boolean hasPathSum(Node root, int target)
+    //    {
+            
+    //        return recur(root, target, 0);
+    //    }
+    //    boolean recur(Node root, int target, int sum)
+    //    {
+    //        if (root == null)
+    //        {
+    //            return false;
+    //        }
+    //        sum += root.data;
+    //        if (root.left == null && root.right == null)
+    //        {
+    //            return sum == target;
+    //        }
+    //        return recur(root.left, target, sum) ||
+    //               recur(root.right, target, sum);
+
+    //    }
+    //}
+}
