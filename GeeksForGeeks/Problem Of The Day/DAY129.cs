3ecd4fedbe785421515a1fae230af54ad7af5f66
Your Name <kumarvipin897946@gmail.com>
2024-07-16 22:00:36 +0530

    129

---
+++ GeeksForGeeks/Problem Of The Day/DAY129.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY129
+    {
+        //Remaining String
+        //problem link =>>https://www.geeksforgeeks.org/problems/remaining-string3515/1
+    }
+    //class Solution
+    //{
+    //    public String printString(String s, char ch, int count)
+    //    {
+    //        // code here
+    //        char[] arr = s.toCharArray();
+    //        StringBuilder sb = new StringBuilder("");
+    //        int cnt = 0;
+    //        int n = arr.length;
+    //        for (int i = 0; i < n; i++)
+    //        {
+    //            if (arr[i] == ch)
+    //            {
+    //                cnt++;
+    //            }
+
+    //            if (cnt == count)
+    //            {
+    //                return s.substring(i + 1);
+    //            }
+
+
+    //        }
+    //        return "";
+    //    }
+    //}
+}
