f58c1f48071220b8e719e2bcbff88cd4e2ee7f4a
Your Name <kumarvipin897946@gmail.com>
2024-07-18 17:16:11 +0530

    131

---
+++ GeeksForGeeks/Problem Of The Day/DAY131.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY131
+    {
+        //Longest alternating subsequence
+        //problem link =>>https://www.geeksforgeeks.org/problems/longest-alternating-subsequence5951/1
+
+    }
+    //class Solution
+    //{
+    //    public int alternatingMaxLength(int[] arr)
+    //    {
+    //        int inc = 1;
+    //        int dec = 1;
+
+    //        for (int i = 1; i < arr.length; i++)
+    //        {
+    //            if (arr[i] > arr[i - 1])
+    //            {
+    //                inc = dec + 1;
+    //            }
+    //            if (arr[i] < arr[i - 1])
+    //            {
+    //                dec = inc + 1;
+    //            }
+    //        }
+
+    //        return Math.max(inc, dec);
+    //    }
+    //}
+}
