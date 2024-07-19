9245c23a6f04b80bd71dabc415ed511dfbd1c73c
Your Name <kumarvipin897946@gmail.com>
2024-07-19 20:02:09 +0530

    132

---
+++ GeeksForGeeks/Problem Of The Day/DAY132.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY132
+    {
+        //Count Smaller elements
+        //problem link=>>https://www.geeksforgeeks.org/problems/count-smaller-elements2214/1
+    }
+    //class Solution
+    //{
+    //    int[] constructLowerArray(int[] arr)
+    //    {
+    //        int[] res = new int[arr.length];
+    //        for (int i = 0; i < arr.length - 1; i++)
+    //        {
+    //            int a = arr[i];
+    //            int c = 0;
+    //            for (int j = i + 1; j < arr.length; j++)
+    //            {
+    //                if (a > arr[j]) c++;
+    //            }
+    //            res[i] = c;
+    //        }
+    //        return res;
+    //    }
+    //}
+}
