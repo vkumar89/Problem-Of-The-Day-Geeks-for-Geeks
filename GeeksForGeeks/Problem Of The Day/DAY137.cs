82763937dabbeb668f55d9bee7c7edfd7c02ce27
Your Name <kumarvipin897946@gmail.com>
2024-07-24 21:04:05 +0530

    137

---
+++ GeeksForGeeks/Problem Of The Day/DAY137.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY137
+    {
+        //Check for BST
+        //probelm link=>>https://www.geeksforgeeks.org/problems/check-for-bst/1
+    }
+    //class Solution
+    //{
+    //    // Function to check whether a Binary Tree is BST or not.
+    //    boolean isBST(Node root)
+    //    {
+    //        // code here.
+    //        return isBSTUtil(root, null, null);
+    //    }
+
+    //    // Helper function
+    //    private boolean isBSTUtil(Node root, Integer min, Integer max)
+    //    {
+    //        if (root == null)
+    //        {
+    //            return true;
+    //        }
+
+    //        if (min != null && root.data <= min || max != null && root.data >= max)
+    //        {
+    //            return false;
+    //        }
+
+    //        return isBSTUtil(root.left, min, root.data) && isBSTUtil(root.right, root.data, max);
+    //    }
+    //}
+}
