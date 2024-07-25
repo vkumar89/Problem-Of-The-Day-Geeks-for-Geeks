7b0052eb57b7655d5daa85f8dcccff5c0219af0a
Your Name <kumarvipin897946@gmail.com>
2024-07-25 16:49:53 +0530

    138

---
+++ GeeksForGeeks/Problem Of The Day/DAY138.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY138
+    {
+        //Array to BST
+        //problem link=>>https://www.geeksforgeeks.org/problems/array-to-bst4443/1
+    }
+    //class Solution
+    //{
+    //    public Node sortedArrayToBST(int[] nums)
+    //    {
+    //        return solve(0, nums.length - 1, nums);
+    //    }
+    //    public Node solve(int l, int r, int[] nums)
+    //    {
+    //        if (l > r)
+    //            return null;
+    //        int mid = l + (r - l) / 2;
+    //        Node root = new Node(nums[mid]);
+    //        root.left = solve(l, mid - 1, nums);
+    //        root.right = solve(mid + 1, r, nums);
+
+    //        return root;
+    //    }
+    //}
+}
