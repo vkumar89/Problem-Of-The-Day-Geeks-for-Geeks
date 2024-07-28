bc328ef9863ecc63a79d0f19da5f40f738496fab
Your Name <kumarvipin897946@gmail.com>
2024-07-28 20:41:43 +0530

    141

---
+++ GeeksForGeeks/Problem Of The Day/DAY141.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY141
+    {
+        //Remove Duplicates
+        //problem link =>>https://www.geeksforgeeks.org/problems/remove-duplicates3034/1
+    }
+    //class Solution
+    //{
+    //    String removeDups(String str)
+    //    {
+    //        // Create a LinkedHashSet to maintain the order of characters
+    //        Set<Character> charSet = new LinkedHashSet<>();
+
+    //        // Convert the string to a character array
+    //        char[] chars = str.toCharArray();
+
+    //        // Add each character to the set (duplicates will be automatically removed)
+    //        for (char c : chars)
+    //        {
+    //            charSet.add(c);
+    //        }
+
+    //        // Create a StringBuilder to construct the result string
+    //        StringBuilder result = new StringBuilder();
+
+    //        // Append each character from the set to the result string
+    //        for (char c : charSet)
+    //        {
+    //            result.append(c);
+    //        }
+
+    //        // Return the result string
+    //        return result.toString();
+
+
+    //    }
+    //}
+}
