f853104c9b41e15f71fb3ca1c4e5faa14b5a8cf6
Your Name <kumarvipin897946@gmail.com>
2024-07-29 20:03:52 +0530

    142

---
+++ GeeksForGeeks/Problem Of The Day/DAY142.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Of_The_Day
+{
+    internal class DAY142
+    {
+        //Row with max 1s
+        //problem link =>>https://www.geeksforgeeks.org/problems/row-with-max-1s0023/1
+    }
+    //class Solution
+    //{
+    //    public int rowWithMax1s(int arr[][])
+    //    {
+    //        int n = arr.length;
+    //        int m = arr[0].length;
+    //        int max = Integer.MIN_VALUE;
+    //        int row = -1;
+    //        for (int i = 0; i < n; i++)
+    //        {
+    //            int count = 0;
+    //            for (int j = 0; j < m; j++)
+    //            {
+    //                if (arr[i][j] == 1)
+    //                {
+    //                    count++;
+    //                }
+    //            }
+    //            if (count > max && count != 0)
+    //            {
+    //                max = count;
+    //                row = i;
+    //            }
+    //        }
+    //        if (max == Integer.MIN_VALUE)
+    //        {
+    //            return -1;
+    //        }
+    //        return row;
+    //    }
+    //}
+
+
+}
